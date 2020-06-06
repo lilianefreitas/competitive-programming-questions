2ee7ca187d4021203b9395e5e49379b5d6374f23
Liliane Freitas <alunalilianefreitas@gmail.com>
2020-06-06 19:04:04 -0300

    Adding

---
+++ uri/1065.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int x, pares;
+
+
+int main() {
+    for(int i = 0; i < 5; i++){
+        scanf("%d", &x);
+
+        if (x % 2 == 0) {
+            pares++;
+        }
+    }
+
+    printf("%d valores pares\n", pares);
+
+    return 0;
+}
