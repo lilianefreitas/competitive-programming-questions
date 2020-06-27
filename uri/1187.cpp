13f0c706e0c26e7ab50f18410c059337eb71dd0e
Liliane Freitas <alunalilianefreitas@gmail.com>
2020-06-26 22:26:55 -0300

    Added 1187.cpp

---
+++ uri/1187.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 12;
+
+int n = 12;
+double m[N][N], somaarea;
+char c;
+
+int main () {
+
+    scanf(" %c", &c);
+
+    for (int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+
+            scanf("%lf", &m[i][j]);
+
+            if (((i+j) < 11) and (i < j)){
+                somaarea += m[i][j];
+
+            }
+        }
+    }
+
+    double resultado;
+
+    if (c == 'S'){
+        resultado = somaarea;
+
+    } else {
+        resultado = somaarea / 30;
+
+    }
+
+    printf("%.1lf\n", resultado);
+
+    return 0;
+}
