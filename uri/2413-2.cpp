645b3f7a4bf7f739bda47bb7082528db5b7525b8
Liliane Freitas <alunalilianefreitas@gmail.com>
2020-11-30 23:33:30 -0300

    Added 2413-2.cpp

---
+++ uri/2413-2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int link3;
+
+int main() {
+    scanf("%d", &link3);
+
+    int link1 = 4 * link3;
+
+    printf("%d\n", link1);
+
+    return 0;
+}
