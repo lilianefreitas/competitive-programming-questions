10abb8df7750fbde962462657595ea640853cb0f
Liliane Freitas <alunalilianefreitas@gmail.com>
2020-11-30 22:56:16 -0300

    Added 1011-2.cpp

---
+++ uri/1011-2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double r;
+double pi = 3.14159;
+
+int main() {
+    scanf("%lf", &r);
+
+    double volume = (4.0/3) * pi * r * r * r;
+
+    printf("VOLUME = %.3lf\n", volume);
+
+    return 0;
+}
