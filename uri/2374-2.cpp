404dc60fb10570c9e624f0d50a37455072c3d582
Liliane Freitas <alunalilianefreitas@gmail.com>
2020-11-30 22:56:04 -0300

    Added 2374-2.cpp

---
+++ uri/2374-2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int q, t;
+
+int main() {
+    scanf("%d%d", &q, &t);
+
+    int diferenca = q - t;
+
+    printf("%d\n", diferenca);
+
+    return 0;
+}
