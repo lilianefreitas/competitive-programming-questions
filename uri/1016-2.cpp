11f07821bfee98309c7ba8b4a3ea310cab83771e
Liliane Freitas <alunalilianefreitas@gmail.com>
2020-11-30 23:21:53 -0300

    Added 1016-2.cpp

---
+++ uri/1016-2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int km;
+
+int main() {
+    scanf("%d", &km);
+
+    int tempo = km * 2;
+
+    printf("%d minutos\n", tempo);
+
+    return 0;
+}
