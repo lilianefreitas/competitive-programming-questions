b7f4e3b3034e3fa90f9e6dc5a83c240339acc335
Liliane Freitas <alunalilianefreitas@gmail.com>
2020-12-04 03:34:13 -0300

    Added 1035-2.cpp

---
+++ uri/1035-2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b, c, d;
+
+int main() {
+    scanf("%d%d%d%d", &a, &b, &c, &d);
+
+    if ((b > c) and (d > a) and (c+d > a+b) and (c > 0) and (d > 0) and (a % 2 == 0)){
+
+        printf("Valores aceitos\n");
+
+    } else {
+
+        printf("Valores nao aceitos\n");
+
+    }
+
+    return 0;
+}
