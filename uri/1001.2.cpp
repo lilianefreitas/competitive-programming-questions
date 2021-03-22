a54742d2f9a81a59e2d8024df88ac9b8c0447fee
Liliane Freitas <alunalilianefreitas@gmail.com>
2021-03-22 14:44:03 -0300

    Added 1001.2.cpp

---
+++ uri/1001.2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b;
+
+int main() {
+    scanf("%d%d", &a, &b);
+
+    int resultado = a + b;
+
+    printf("X = %d", resultado);
+    
+    return 0;
+}
