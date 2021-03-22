b3fc6be8f9ccfb617f6bc86055fc56c3b7c28f2e
Liliane Freitas <alunalilianefreitas@gmail.com>
2021-03-22 14:54:00 -0300

    Added 1003.2.cpp

---
+++ uri/1003.2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// declarando as variáveis
+int a, b;
+
+int main() {
+    
+    // pegando os valores e salvando nas variáveis
+    scanf("%d%d", &a, &b);
+
+    // fazendo a operação necessária
+    int soma = a + b;
+
+    // imprimindo o resultado
+    // colocar barra n depois do %d
+    printf("SOMA = %d", soma);
+
+    return 0;
+}
